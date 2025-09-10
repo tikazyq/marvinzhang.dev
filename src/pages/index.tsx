16ed589c93c99aed4ea1fc021c75a81a2405225c
Marvin Zhang <tikazyq@163.com>
2025-09-10 22:15:53 +0800

    Add locale-based redirection to blog page in Home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import { Redirect } from '@docusaurus/router';
 
 export default function Home(): React.JSX.Element {
+  const {i18n} = useDocusaurusContext();
+  const currentLocale = i18n.currentLocale as 'en' | 'zh';
+  if (currentLocale === 'zh') {
+    return <Redirect to="/zh/blog" />;
+  }
   return <Redirect to="/blog" />;
 }
